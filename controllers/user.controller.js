d93353ef97b19332e2cd2e076e9244664258fe40
devmaxwel <eng.maxwel.ochieng@gmail.com>
2022-02-17 22:34:34 +0300

    email validator

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,79 +1,91 @@
 import userModel from "../models/user.model.js";
 import asyncHandler from "express-async-handler";
 import { generateToken } from "../utils/jwt.util.js";
+import validator from "email-validator";
 
-export const registerUser = asyncHandler(async(req, res) => {
-
-  const { username, email, password, profilePic, admin} = req.body;
+export const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password, profilePic, admin } = req.body;
 
   const ifUserExist = await userModel.findOne({ email });
 
-  if(ifUserExist){
-    res.status(400).json({errorMessage :"email is already in use please check and try again"})
+  if (ifUserExist) {
+    res
+      .status(400)
+      .json({
+        errorMessage: "email is already in use please check and try again",
+      });
   }
 
-  const user = await userModel.create({
-    username,
-    email,
-    password,
-    profilePic,
-    admin,
-  });
-
-  const token = generateToken(user._id)
-
-  if (user) {
-    res.json({
-      _id:user._id,
-      username: user.username, 
-      email: user.email,
-      admin:user.admin,
-      profilePic:user.profilePic,
-      token:token,
+  if (validator.validate(req.body.email)) {
+    const user = await userModel.create({
+      username,
+      email,
+      password,
+      profilePic,
+      admin,
     });
-    
+    const token = generateToken(user._id);
 
-  } else {
-
-    res.status(500).json({message: "an error occurred while processing your request, please try again "});
+    if (user) {
+      res.json({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        admin: user.admin,
+        profilePic: user.profilePic,
+        token: token,
+      });
+    } else {
+      res
+        .status(500)
+        .json({
+          message:
+            "an error occurred while processing your request, please try again ",
+        });
+    }
+  } else{
+    res
+    .status(500)
+    .json({
+      errorMessage: "Invalid Email Type",
+    });
   }
-
 });
 
-export const loginUser = asyncHandler(async(req, res) => {
+export const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await userModel.findOne({ email });
 
-  const token = generateToken(user._id)
+  const token = generateToken(user._id);
 
-  if(user && (await user.matchPassword(password))){
+  if (user && (await user.matchPassword(password))) {
     res.json({
-      _id:user._id,
+      _id: user._id,
       username: user.username,
       email: user.email,
-      profilePic:user.profilePic,
-      admin:user.admin,
-      token:token,
-
+      profilePic: user.profilePic,
+      admin: user.admin,
+      token: token,
     });
-
   } else {
-    res.status(400).json({errorMessage :"invalid email or password please check and try again!" })
+    res
+      .status(400)
+      .json({
+        errorMessage: "invalid email or password please check and try again!",
+      });
   }
-
 });
 
-export const passwordReset= asyncHandler(async(req, res) => {
-     const { email,} = req.body;
+export const passwordReset = asyncHandler(async (req, res) => {
+  const { email } = req.body;
 
-     const userExist = await userModel.findOne({email})
+  const userExist = await userModel.findOne({ email });
 
-     if(userExist){
-       res.json({message: "A reset password link has been sent to your email"});
-       await SendGridHelper.sendPasswordResetEmail(email);
-     }else{
-      res.status(400)
-      
-     }
+  if (userExist) {
+    res.json({ message: "A reset password link has been sent to your email" });
+    await SendGridHelper.sendPasswordResetEmail(email);
+  } else {
+    res.status(400);
+  }
 });
