551108f1cacf95dea08cc035c066335dff84673d
Maxwel Ochieng <64557216+devmaxwel@users.noreply.github.com>
2022-06-07 22:32:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ dotenv.config()
 const app = express()
 
 app.use(express.json())
-app.use(cors());
-
-
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.listen(process.env.PORT,  async() => {
 
       console.info(`Server is running at http://localhost:${process.env.PORT}`)
